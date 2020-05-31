efc66a3a3a8fda6143881ccfabf4bc37148e30c4
kaitlyncook <kaitlyncook@g.harvard.edu>
2020-05-31 17:49:48 -0400

    Initial commit

---
+++ data-application/data-application.R
@@ -0,0 +1,192 @@
+#-----------------------------------------------------------------------------------------------------#
+#   Applied Example: Simulated Botswana Combination Prevention Project (BCPP) Data                    #
+#-----------------------------------------------------------------------------------------------------#
+
+source("../cp-simulation-functions.R")
+
+
+#-----------------------------------------------------------------------------------------------------
+# Data and Conditional Power Summary Functions
+#-----------------------------------------------------------------------------------------------------
+
+### Summarizes the number of events, contibuted person-time, and estimated hazard within each trial arm
+  # data           dataframe    interval-censored interim data
+  # trunc.time     numeric      (optional) user-specified maximum knot point for the hazard function approximation
+summarize_events <- function(data, trunc.time = NULL){
+  n.events.i <- sum(data$right != Inf & data$treat==1) 
+  n.events.c <- sum(data$right != Inf & data$treat==0)
+  f <- est_frail(data, "lognormal")
+  curves <- est_curve(data, "lognormal", f$theta)
+  l.t <- est_hazard(curves$treat, end.point=trunc.time)*52
+  l.c <- est_hazard(curves$control, end.point=trunc.time)*52
+  hr <- l.t/l.c
+  out <- matrix(c(n.events.i, n.events.c, round(l.t, 5), round(l.c, 5), round(hr, 3), f[[2]]), nrow=1)
+  colnames(out) <- c("events.in.treated", "events.in.control", " hazard.t", " hazard.c", "HR", "f.theta")
+  print(out)
+}
+
+### Summarizes the conditional power and data generation results for a particular multiplicative change in the 
+### future conditional hazard functions
+  # cp.out         list         est_cp() object
+  # l0.delta       numeric      multiplicative change in the hazard within the control arm
+  # l1.delta       numeric      multiplicative change in the hazard within the intervention arm
+  # digits         numeric      number of decimal places to be printed in the summary table
+  # mean           character    type of mean to report ("arithmetic"/"geometric")
+summarize_results <- function(cp.out, l0.delta, l1.delta, digits=3, mean="arithmetic"){
+  out <- matrix(NA, nrow=2, ncol=10)
+  out[, 1] <- c(l0.delta, ".")
+  out[, 2] <- c(round(cp.out$int.hazard$control*52*l0.delta, digits), ".")
+  out[, 3] <- c(l1.delta/l0.delta, ".")
+  out[, 4] <- c(round((cp.out$int.hazard$treat*l1.delta)/(cp.out$int.hazard$control*l0.delta), digits), ".")
+  out[, 5] <- c(round(mean(cp.out$full.events$treat), 1), paste0("(", min(cp.out$full.events$treat), ", ", max(cp.out$full.events$treat), ")"))
+  out[, 6] <- c(round(mean(cp.out$full.events$control), 1), paste0("(", min(cp.out$full.events$control), ", ", max(cp.out$full.events$control), ")"))
+  out[, 7] <- c(switch(mean, "arithmetic"=round(mean(cp.out$full.hr), digits), "geometric"=round(exp(mean(log(cp.out$full.hr)))), digits), round(sd(cp.out$full.hr), digits))
+  out[, 8] <- c(round(mean(cp.out$full.person.time$treat), 1), round(sd(cp.out$full.person.time$treat), 1))
+  out[, 9] <- c(round(mean(cp.out$full.person.time$control), 1), round(sd(cp.out$full.person.time$control), 1))
+  out[, 10] <- c(cp.out$cp, ".")
+  colnames(out) <- c("l.delta", "l.2", "hr.delta", "HR.2", "events.in.t", "events.in.c", "full.HR", "time.in.t", "time.in.c", "cp")
+  out <- as.data.frame(out)
+  print(noquote(out), row.names=FALSE)
+}
+
+
+#-----------------------------------------------------------------------------------------------------
+# BCPP Conditional Power Analysis
+#-----------------------------------------------------------------------------------------------------
+
+# Reading in the interim data
+int.data <- read.csv("simulated-bcpp.csv")
+int.data$proj <- ifelse(int.data$right == Inf & int.data$left > 78-4, 1, 0)
+int.data$start.date <- rep(0, nrow(int.data))
+summarize_events(int.data, 78)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 1; \Delta_HR = 1
+#-----------------------------------------------------------------
+
+set.seed(101)
+start.1.1 <- Sys.time()
+cp.1.1 <- est_cp(int.data, 78, c(52, 104, 156), 4, 1, 1, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.1.1 <- Sys.time()
+
+end.1.1-start.1.1
+summarize_results(cp.1.1, 1, 1)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 1; \Delta_HR = 0.8
+#-----------------------------------------------------------------
+
+set.seed(102)
+start.1.0.8 <- Sys.time()
+cp.1.0.8 <- est_cp(int.data, 78, c(52, 104, 156), 4, 1, 0.8, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.1.0.8 <- Sys.time()
+
+end.1.0.8 - start.1.0.8
+summarize_results(cp.1.0.8, 1, 0.8)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 1; \Delta_HR = 0.9
+#-----------------------------------------------------------------
+
+set.seed(103)
+start.1.0.9 <- Sys.time()
+cp.1.0.9 <- est_cp(int.data, 78, c(52, 104, 156), 4, 1, 0.9, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.1.0.9 <- Sys.time()
+
+end.1.0.9 - start.1.0.9
+summarize_results(cp.1.0.9, 1, 0.9)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 1; \Delta_HR = 1.1
+#-----------------------------------------------------------------
+
+set.seed(104)
+start.1.1.1 <- Sys.time()
+cp.1.1.1 <- est_cp(int.data, 78, c(52, 104, 156), 4, 1, 1.1, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.1.1.1 <- Sys.time()
+
+end.1.1.1-start.1.1.1
+summarize_results(cp.1.1.1, 1, 1.1)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 1; \Delta_HR = 1.2
+#-----------------------------------------------------------------
+
+set.seed(105)
+start.1.1.2 <- Sys.time()
+cp.1.1.2 <- est_cp(int.data, 78, c(52, 104, 156), 4, 1, 1.2, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.1.1.2 <- Sys.time()
+
+end.1.1.2 - start.1.1.2
+summarize_results(cp.1.1.2, 1, 1.2)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 0.9; \Delta_HR = 1
+#-----------------------------------------------------------------
+
+set.seed(106)
+start.0.9.1 <- Sys.time()
+cp.0.9.1 <- est_cp(int.data, 78, c(52, 104, 156), 4, 0.9, 0.9*1, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.0.9.1 <- Sys.time()
+
+end.0.9.1 - start.0.9.1
+summarize_results(cp.0.9.1, 0.9, 0.9*1)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 0.9; \Delta_HR = 0.8
+#-----------------------------------------------------------------
+
+set.seed(107)
+start.0.9.0.8 <- Sys.time()
+cp.0.9.0.8 <- est_cp(int.data, 78, c(52, 104, 156), 4, 0.9, 0.9*0.8, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.0.9.0.8 <- Sys.time()
+
+end.0.9.0.8 - start.0.9.0.8
+summarize_results(cp.0.9.0.8, 0.9, 0.9*0.8)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 0.9; \Delta_HR = 0.9
+#-----------------------------------------------------------------
+
+set.seed(108)
+start.0.9.0.9 <- Sys.time()
+cp.0.9.0.9 <- est_cp(int.data, 78, c(52, 104, 156), 4, 0.9, 0.9*0.9, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.0.9.0.9 <- Sys.time()
+
+end.0.9.0.9 - start.0.9.0.9
+summarize_results(cp.0.9.0.9, 0.9, 0.9*0.9)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 0.9; \Delta_HR = 1.1
+#-----------------------------------------------------------------
+
+set.seed(109)
+start.0.9.1.1 <- Sys.time()
+cp.0.9.1.1 <- est_cp(int.data, 78, c(52, 104, 156), 4, 0.9, 0.9*1.1, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.0.9.1.1 <- Sys.time()
+
+end.0.9.1.1 - start.0.9.1.1
+summarize_results(cp.0.9.1.1, 0.9, 0.9*1.1)
+
+
+#-----------------------------------------------------------------
+# Analysis: \Delta_lambda = 0.9; \Delta_HR = 1.2
+#-----------------------------------------------------------------
+
+set.seed(110)
+start.0.9.1.2 <- Sys.time()
+cp.0.9.1.2 <- est_cp(int.data, 78, c(52, 104, 156), 4, 0.9, 0.9*1.2, matched_analysis, 0.05, 1000, cens=0.002, trunc.time=78)
+end.0.9.1.2 <- Sys.time()
+
+end.0.9.1.2 - start.0.9.1.2
+summarize_results(cp.0.9.1.2, 0.9, 0.9*1.2)
+
---
+++ simulation-study/figure-table-generation.R
@@ -0,0 +1,188 @@
+#-----------------------------------------------------------------------------------------------------#
+#   Simulation Studies: Producing the Figures and Tables in Cook and Wang (2020)                      #
+#-----------------------------------------------------------------------------------------------------#
+
+source("analysis-functions.R")
+
+
+#-----------------------------------------------------------------------------------------------------
+# Reading in the Simulation Results
+#-----------------------------------------------------------------------------------------------------
+
+## Unmatched Design Results
+
+unmatched.low <- read.csv("raw-results/unmatched_0.001.csv")
+unmatched.high <- read.csv("raw-results/unmatched_0.01.csv")
+
+
+## Matched Design Results
+
+matched.low <- read.csv("raw-results/matched_0.001.csv")
+matched.high <- read.csv("raw-results/matched_0.01.csv")
+
+
+## Sensitivity Analysis Results
+
+moderate.m <- read.csv("moderate_m_0.001_0.06.csv")
+large.m <- read.csv("large_m_0.001_0.06.csv")
+frequency <- read.csv("freq_0.001_0.06.csv")
+re <- read.csv("re_0.001_0.25.csv")
+
+
+#-----------------------------------------------------------------------------------------------------
+# Main Manuscript: Figures & Tables
+#-----------------------------------------------------------------------------------------------------
+
+## Figure 3: Conditional Power Curves
+
+# Matched CRT design
+matched.l <- format_plot_data(matched.low[abs(log(matched.low$hr)) < 0.9])
+matched.h <- format_plot_data(matched.high)
+p1 <- visualize_cp_curves(matched.l)
+p2 <- visualize_cp_curves(matched.h)
+matched.p <- grid.arrange(p1, p2, nrow=1, ncol=2, top = textGrob("Matched CRT Design", gp=gpar(fontsize=26)))
+
+# Unmatched CRT design
+unmatched.l <- format_plot_data(unmatched.low[unmatched.low$f.theta==0.06 & abs(log(unmatched.low$hr)) < 0.9, ])
+unmatched.h <- format_plot_data(unmatched.high[unmatched.high$f.theta==0.22, ])
+p3 <- visualize_cp_curves(unmatched.l)
+p4 <- visualize_cp_curves(unmatched.h)
+unmatched.p <- grid.arrange(p3, p4, nrow=1, ncol=2, top = textGrob("Unmatched CRT Design", gp=gpar(fontsize=26)))
+
+figure.3 <- grid.arrange(matched.p, unmatched.p, nrow=2, ncol=1)
+
+
+## Figure 4: Classification Accuracy
+
+figure.4 <- visualize_accuracy_matched(rbind(matched.low, matched.high), 
+                                       rbind(unmatched.low[unmatched.low$f.theta==0.06, ], 
+                                             unmatched.high[unmatched.high$f.theta==0.22,]))
+
+
+## Summary of Classification Performance
+
+# Matched CRT design
+summarize_accuracy(rbind(matched.low[!(round(log(matched.low$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)),],
+                         matched.high[!(round(log(matched.high$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)), ]))
+
+# Unmatched CRT design
+summarize_accuracy(rbind(unmatched.low[unmatched.low$f.theta==0.06 & !(round(log(unmatched.low$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)),],
+                         unmatched.high[unmatched.high$f.theta==0.22 & !(round(log(unmatched.high$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)), ]))
+
+# Overall
+summarize_accuracy(rbind(matched.low[!(round(log(matched.low$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)),],
+                         matched.high[!(round(log(matched.high$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)), ],
+                         unmatched.low[unmatched.low$f.theta==0.06 & !(round(log(unmatched.low$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)),],
+                         unmatched.high[unmatched.high$f.theta==0.22 & !(round(log(unmatched.high$hr), 2) %in% c(-0.15, -0.05, 0.05, 0.15)), ]))
+
+
+#-----------------------------------------------------------------------------------------------------
+# Supporting Information: Web Figures & Tables
+#-----------------------------------------------------------------------------------------------------
+
+## Web Figures 1 & 2: Performance of the Approximated Cluster-Conditional Survival Function
+
+# Exponential failure times
+el1 <- curve_comparison_exp(0.001, 0.22, 208)
+el2 <- curve_comparison_exp(0.001, 0.45, 208)
+el3 <- curve_comparison_exp(0.001, 0.69, 208)
+eh1 <- curve_comparison_exp(0.01, 0.22, 208)
+eh2 <- curve_comparison_exp(0.01, 0.45, 208)
+eh3 <- curve_comparison_exp(0.01, 0.69, 208)
+web.figure.1 <- grid.arrange(arrangeGrob(grobs=list(el1, eh1), top=textGrob("k=0.50", gp=gpar(fontsize=25)), ncol=1),
+                             arrangeGrob(grobs=list(el2, eh2), top=textGrob("k=0.75", gp=gpar(fontsize=25)), ncol=1),
+                             arrangeGrob(grobs=list(el3, eh3), top=textGrob("k=1.00", gp=gpar(fontsize=25)), ncol=1),
+                             ncol=3)
+
+# Weibull failure times
+wl1 <- curve_comparison_weib(get_param_lambda(0.5, 0.001, 208), 0.5, 0.22, 208)
+wl2 <- curve_comparison_weib(get_param_lambda(0.5, 0.001, 208), 0.5, 0.45, 208)
+wl3 <- curve_comparison_weib(get_param_lambda(0.5, 0.001, 208), 0.5, 0.69, 208)
+wh1 <- curve_comparison_weib(get_param_lambda(2, 0.01, 208), 2, 0.22, 208)
+wh2 <- curve_comparison_weib(get_param_lambda(2, 0.01, 208), 2, 0.45, 208)
+wh3 <- curve_comparison_weib(get_param_lambda(2, 0.01, 208), 2, 0.69, 208)
+web.figure.2 <- grid.arrange(arrangeGrob(grobs=list(wl1, wh1), top=textGrob("k=0.50", gp=gpar(fontsize=25)), ncol=1),
+                             arrangeGrob(grobs=list(wl2, wh2), top=textGrob("k=0.75", gp=gpar(fontsize=25)), ncol=1),
+                             arrangeGrob(grobs=list(wl3, wh3), top=textGrob("k=1.00", gp=gpar(fontsize=25)), ncol=1),
+                             ncol=3)
+
+
+## Web Figures 3 & 4: Individual Study-Specific Concordance
+
+web.figure.3 <- combined_distribution_mosaic_plots(matched.low, beta=list(-0.1, -0.2, -0.3))
+web.figure.4 <- combined_distribution_mosaic_plots(unmatched.low[unmatched.low$f.theta==0.06,], beta=list(-0.1, -0.2, -0.3))
+
+
+## Web Figures 5 & 6: Data Generation (Bias + MSE)
+
+web.figure.5 <- visualize_data_gen(rbind(matched.low, matched.high), beta=c(-0.2, 0, 0.2))
+web.figure.6 <- visualize_data_gen(rbind(unmatched.low[unmatched.low$f.theta==0.06,],
+                                         unmatched.high[unmatched.high$f.theta==0.22, ]), beta=c(-0.2, 0, 0.2))
+
+
+## Web Table 2: Data Generation
+
+summarize_data_gen(rbind(matched.low, matched.high), beta=c(-0.2, 0, 0.2))
+summarize_data_gen(rbind(unmatched.low[unmatched.low$f.theta==0.06, ], 
+                         unmatched.high[unmatched.high$f.theta==0.22, ]), beta=c(-0.2, 0, 0.2))
+
+
+## Web Figure 7: Full Conditional Power Curves (Unmatched CRT Design)
+
+l.i  <- format_plot_data(unmatched.low[unmatched.low$f.theta==0 & abs(log(unmatched.low$hr)) < 0.5, ], TRUE)
+l.mi <- format_plot_data(unmatched.low[unmatched.low$f.theta==0.06 & abs(log(unmatched.low$h)) < 0.9, ], TRUE)
+l.mo <- format_plot_data(unmatched.low[unmatched.low$f.theta==0.22, ], TRUE)
+h.i  <- format_plot_data(unmatched.high[unmatched.high$f.theta==0 & abs(log(unmatched.high$hr)) < 0.3, ], TRUE)
+h.mi <- format_plot_data(unmatched.high[unmatched.high$f.theta==0.06 & abs(log(unmatched.high$hr)) < 0.7, ], TRUE)
+h.mo <- format_plot_data(unmatched.high[unmatched.high$f.theta==0.22, ], TRUE)
+
+cp1 <- visualize_cp_curves(l.i)
+cp2 <- visualize_cp_curves(l.mi)
+cp3 <- visualize_cp_curves(l.mo)
+cp4 <- visualize_cp_curves(h.i)
+cp5 <- visualize_cp_curves(h.mi)
+cp6 <- visualize_cp_curves(h.mo)
+
+web.figure.7 <- grid.arrange(cp1, cp4, cp2, cp5, cp3, cp6, nrow=3, ncol=2)
+
+
+## Web Figure 8: Classification Accuracy (Unmatched CRT Design)
+
+web.figure.8 <- visualize_accuracy_incidence(rbind(unmatched.low, unmatched.high))
+
+
+## Web Table 3: Cluster Size Sensitivity Analysis
+
+setting.1 <- format_plot_data(moderate.m, TRUE) 
+setting.2 <- format_plot_data(large.m, TRUE) 
+
+
+## Web Table 4: Frequency of Monitoring Visits Sensitivity Analysis
+
+temp <- format_plot_data(frequency, TRUE)
+
+
+## Web Table 5: Misspecification of the Frailty Distribution Sensitivity Analysis
+
+# Table creation
+temp <- matrix(NA, ncol=5, nrow=3); i <- 1
+for (h in sort(unique(re$hr))){
+  dat <- re[re$hr==h, ]
+  results.mean <- apply(cbind(dat$power, dat$ln.cp, dat$gamma.cp, dat$known.cp), 2, mean)
+  results.sd <- apply(cbind(dat$power, dat$ln.cp, dat$gamma.cp, dat$known.cp), 2, sd)
+  temp[i, 1] <- round(log(h), 3)
+  temp[i, 2:5] <- c(paste0(round(results.mean[1], 3), " (", round(results.sd[1], 3), ")"),
+                    paste0(round(results.mean[2], 3), " (", round(results.sd[2], 3), ")"), 
+                    paste0(round(results.mean[3], 3), " (", round(results.sd[3], 3), ")"),
+                    paste0(round(results.mean[4], 3), " (", round(results.sd[4], 3), ")"))
+  i <- i + 1
+}
+colnames(temp) <- c("Beta", "Power", "CP (Lognormal)", "CP (Gamma)", "CP (All Known)")
+print(noquote(temp), row.names=FALSE)
+
+# Correspondence between interim futility classifications under gamma and lognormal frailties
+gamma.futile <- ifelse(re$gamma.cp < 0.2, 1, 0)
+ln.futile <- ifelse(re$ln.cp < 0.2, 1, 0)
+sum(gamma.futile==ln.futile)
+mean(gamma.futile==ln.futile)
+
